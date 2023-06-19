b068bbe0f0971f56fc8cc1ded34b17a8093ac605
John Poikilidis <akiskawi@gmail.com>
2023-06-19 12:17:00 +0300

    UserView working execpt the workout list

--- src/components/user/UserView.jsx
+++ src/components/user/UserView.jsx
@@ -2,9 +2,16 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api";
 
-const UserView = ({singleUser}) => {
+const UserView = () => {
   const { id } = useParams();
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    stats: { height: "", bodyFat: "", weight: "" },
+    goals: { bodyFat: "", weight: "" },
+  });
   useEffect(() => {
     api
       .get(`/users/${id}`)
@@ -12,9 +19,36 @@ const UserView = ({singleUser}) => {
       .catch((err) => console.log(err));
   }, []);
 
-//   if (user) {
-//     return <span className="loader"></span>;
-//   }
-  return <div>UserView</div>;
+  if (user.id === "") {
+    return <span className="loader"></span>;
+  }
+  return (
+    <>
+      <h2>
+        {user.firstName} {user.lastName}
+      </h2>
+      <div>
+        <p>Contact: {user.email}</p>
+        <div style={{display:"grid",gridTemplateColumns:"1fr 1fr",marginTop:"2em"}}>
+          <div>
+            <p>Current Stats</p>
+            <ul>
+              <li>Height: {user.stats.height}</li>
+              <li>Weight: {user.stats.weight}</li>
+              <li>Bodyfat: {user.stats.bodyFat}</li>
+            </ul>
+          </div>
+          <div>
+            <p>Goal Stats</p>
+            <ul>
+              <li>Weight: {user.goals.weight}</li>
+              <li>Bodyfat: {user.goals.bodyFat}</li>
+            </ul>
+          </div>
+        </div>
+        <p>Hello Workouts here</p>
+      </div>
+    </>
+  );
 };
 export default UserView;
